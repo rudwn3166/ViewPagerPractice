f7ec16740d0c4a9806a2816c0f59bd401465f769
Joo <rudwn3166@naver.com>
2021-10-26 21:53:09 +0900

    메인 xml에 어댑터를 연결해줬다 중간에 코틀린 extensions를 깜빡해서 id가 검색되지 않았다 잊지 말자

--- app/src/main/java/com/example/viewpagerpractice/MainActivity.kt
+++ app/src/main/java/com/example/viewpagerpractice/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.viewpagerpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import com.example.viewpagerpractice.adapters.FragmentAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mFragmentAdapter : FragmentAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFragmentAdapter= FragmentAdapter(supportFragmentManager)
+        mainViewpager.adapter=mFragmentAdapter
+
     }
 }
