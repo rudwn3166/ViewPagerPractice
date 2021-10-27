db66a2b15fde244a82da640f896408601b07d64a
Joo <rudwn3166@naver.com>
2021-10-27 20:17:28 +0900

    offscreenpagelimit및 주석 작업 했다

--- app/src/main/java/com/example/viewpagerpractice/MainActivity.kt
+++ app/src/main/java/com/example/viewpagerpractice/MainActivity.kt
@@ -14,8 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        프레그먼트 어댑터에 메인 xml어댑터와 연결 해주는 작업
+//        supportFragmentManager를 넣어줘야함
         mFragmentAdapter= FragmentAdapter(supportFragmentManager)
         mainViewpager.adapter=mFragmentAdapter
+        
+//        페그먼트가 많아지면 화면을 부드럽게 적용해주는 기능
+        mainViewpager.offscreenPageLimit= 3
 
     }
 }
